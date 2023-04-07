6e42c9503342c7c20528035488abc6717610c039
Sanzida-Akter-Chadne-17 <118118214+Sanzida-Akter-Chadne-17@users.noreply.github.com>
2023-04-07 19:04:01 +0600

    Add files via upload

---
+++ text.js
@@ -0,0 +1,114 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('load scroll',function(){
+
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if($(window).scrollTop() > 60){
+            $('header .header-2').addClass('header-active');
+        }else{
+            $('header .header-2').removeClass('header-active');
+        }
+
+        $('section').each(function(){
+
+            let height = $(this).height();
+            let offset = $(this).offset().top - 200;
+            let top = $(window).scrollTop();
+            let id = $(this).attr('id');
+
+            if(top >= offset && top < offset + height){
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+
+        });
+
+    });
+
+    $('.controls .buttons').click(function(){
+
+        $(this).addClass('button-active').siblings().removeClass('button-active');
+
+        let filter = $(this).attr('data-filter');
+        if(filter == 'all'){
+            $('.dish .image').show(400);
+        }else{
+            $('.dish .image').not('.'+filter).hide(200);
+            $('.dish .image').filter('.'+filter).show(400);
+        }
+
+       
+
+    });
+
+    $('.controls .buttons').click(function(){
+
+      $(this).addClass('button-active').siblings().removeClass('button-active');
+
+      let filter = $(this).attr('data-filter');
+      if(filter == 'all'){
+          $('.cuisines .image').show(400);
+      }else{
+          $('.cuisines .image').not('.'+filter).hide(200);
+          $('.cuisines .image').filter('.'+filter).show(400);
+      }
+
+    });
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
+
+});
+
